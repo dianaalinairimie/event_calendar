0d758eb11201f42ceca0d4be410eb97ef45ccbaa
Diana Irimie <irimiediana.id@gmail.com>
2017-03-24 22:15:54 +0200

    Move events settings from configuration to content.

---
+++ src/Controller/EventsController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\event_calendar\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Returns responses for event calendar module administrative routes.
+ */
+class EventsController extends ControllerBase {
+
+  /**
+   * The form builder.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * Constructs an EventsController object.
+   *
+   * @param \Drupal\Core\Form\FormBuilderInterface $form_builder
+   *   The form builder.
+   */
+  public function __construct(FormBuilderInterface $form_builder) {
+    $this->formBuilder = $form_builder;
+  }
+
+  /**
+   *
+   */
+  public function eventsPage() {
+  }
+
+}
