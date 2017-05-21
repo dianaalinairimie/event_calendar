18a91e35b95f575b2c1969af3cffe25e1e4be5b9
Diana Irimie <irimiediana.id@gmail.com>
2017-05-21 14:18:18 +0300

    Add events with the date clicked.

--- event_calendar_popup/js/event-calendar-popup.js
+++ event_calendar_popup/js/event-calendar-popup.js
@@ -5,25 +5,48 @@
         attach: function (context, settings) {
             $('table.full tr td, table.mini tr td', context).click(function (e) {
                     e.preventDefault();
+
+                    $('a').click(function (event) {
+                        event.stopPropagation();
+                    });
+
                     $('.add-event-button').click();
                     // Gets clicked date.
                     var current_date = e.target.getAttribute('date-date');
                     if (current_date === null) {
-                        current_date = e.target.parentNode.getAttribute('date-date');
+                        current_date = e.target.closest('td').getAttribute('date-date');
+                        if (current_date === null) {
+                            current_date = formatDate(Date());
+                            console.log(current_date);
+                        }
                     }
 
-                    // Add simple modal
+                    var afterLoaded = function () {
+                        // code to execute once elements are in place
+                        $(".form-date").val(current_date);
+                    };
 
-                    // var $myDialog = $('<div> Simple modal ' + current_date + '</div>').appendTo('body');
-                    // Drupal.dialog($myDialog, {
-                    //     title: 'Add event',
-                    //     buttons: [{
-                    //         text: 'Close',
-                    //         click: function() {
-                    //             $(this).dialog('close');
-                    //         }
-                    //     }]
-                    // }).showModal();
+                    var checkLoaded = function () {
+                        var interval = setInterval(function () {
+                            if (document.getElementsByClassName("ui-dialog").length) {
+                                clearInterval(interval);
+                                afterLoaded();
+                            }
+                        }, 1000);
+                    };
+                    checkLoaded();
+
+                    function formatDate(date) {
+                        var d = new Date(date),
+                            month = '' + (d.getMonth() + 1),
+                            day = '' + d.getDate(),
+                            year = d.getFullYear();
+
+                        if (month.length < 2) month = '0' + month;
+                        if (day.length < 2) day = '0' + day;
+
+                        return [year, month, day].join('-');
+                    }
                 }
             );
         }
